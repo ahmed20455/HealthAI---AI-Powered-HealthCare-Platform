d5c549e5a852425e04d2308d27ab220204372677
Ahmed Abdul Faheem <100959005+ahmed20455@users.noreply.github.com>
2025-06-27 15:12:25 +0530

    Create voice.tsx

---
+++ app/emer/voice.tsx
@@ -0,0 +1,9 @@
+import GeminiVoiceAssistant from "@/components/voiceass";
+
+export default function HomePage() {
+  return (
+    <main className="min-h-screen bg-gray-100 p-4">
+      <GeminiVoiceAssistant />
+    </main>
+  )
+}
